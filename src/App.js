b25fbf7025e7d38e2699b13035cc4c88e521d3fd
Abdul-Sami <iamsamideveloper@gmail.com>
2024-03-12 09:40:26 +0500

    Software Company Blue Theme in blue theme

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Routers from './routers'
+
+// Swiper CSS
+import "swiper/css";
+import 'swiper/css/navigation';
+
+//bootstrap
+import "bootstrap/dist/js/bootstrap.bundle";
+
+//Modal Video
+import "react-modal-video/scss/modal-video.scss";
+
+import "./assets/css/main.css"
+
+function App() {
+  return (
+    <>
+      <Routers />
+    </>
+  )
+}
+
+export default App
